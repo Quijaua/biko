253cc0d04a9e05825022f192d1c4a43a65b316f8
Rafael Bantu <rafaelgomes@quijaua.com.br>
2020-01-29 11:23:21 -0300

    correção do título do formulário

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Cadastro') }}</div>
+                <div class="card-header">{{ __('Pré-cadastro') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
