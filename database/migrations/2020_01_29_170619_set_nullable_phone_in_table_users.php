8f16ace68d6f0d5b1e8976b35cbd3d4703175491
Alexandre Tucunduva <motive.moi@gmail.com>
2020-01-29 14:08:13 -0300

    Altera a coluna phone da tabela users para NULL

--- database/migrations/2020_01_29_170619_set_nullable_phone_in_table_users.php
+++ database/migrations/2020_01_29_170619_set_nullable_phone_in_table_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPhoneFieldToUsersTable extends Migration
+class SetNullablePhoneInTableUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddPhoneFieldToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-		        $table->bigInteger('phone');
+          $table->bigInteger('phone')->nullable()->change();
         });
     }
 
@@ -26,7 +26,7 @@ class AddPhoneFieldToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-		        $table->dropColumn('phone');
+            //
         });
     }
 }
