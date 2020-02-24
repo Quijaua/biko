3a68be23e2d1154075d00de04242a8f1d25d1432
Rafael Bantu <rafaelgomes@quijaua.com.br>
2020-02-24 14:44:24 -0300

    Gráfico por Raça

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,6 +17,11 @@ body {
   height: 350px;
 }
 
+#chart-genero {
+  width: 800px;
+  height: 450px;
+}
+
     </style>
 
 
@@ -37,6 +42,7 @@ body {
 
     @php
     $cadastros = DB::select('Select count(*) as qtd, DATE_FORMAT(created_at,"%Y-%m-%d") as dia FROM users GROUP BY dia');
+    $generos = DB::select('Select count(*) as qtd, IF(Raca IS NULL or Raca = "", "Sem definição", Raca) as raca FROM alunos GROUP BY raca');
     @endphp
 
 
@@ -113,5 +119,87 @@ chart.scrollbarX.parent = chart.bottomAxesContainer;
 dateAxis.start = 0.79;
 dateAxis.keepSelection = true;
   </script>
+
+
+
+
+
+<div id="chart-genero"></div>
+<script id="INLINE_PEN_JS_ID">
+    /**
+ * ---------------------------------------
+ * This demo was created using amCharts 4.
+ * 
+ * For more information visit:
+ * https://www.amcharts.com/
+ * 
+ * Documentation is available at:
+ * https://www.amcharts.com/docs/v4/
+ * ---------------------------------------
+ */
+
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
+
+// Create chart instance
+var chart = am4core.create("chart-genero", am4charts.PieChart);
+
+// Add and configure Series
+var pieSeries = chart.series.push(new am4charts.PieSeries());
+pieSeries.dataFields.value = "litres";
+pieSeries.dataFields.category = "country";
+
+// Let's cut a hole in our Pie chart the size of 30% the radius
+chart.innerRadius = am4core.percent(30);
+
+// Put a thick white border around each Slice
+pieSeries.slices.template.stroke = am4core.color("#fff");
+pieSeries.slices.template.strokeWidth = 2;
+pieSeries.slices.template.strokeOpacity = 1;
+pieSeries.slices.template
+// change the cursor on hover to make it apparent the object can be interacted with
+.cursorOverStyle = [
+{
+  "property": "cursor",
+  "value": "pointer" }];
+
+
+
+pieSeries.alignLabels = false;
+pieSeries.labels.template.bent = true;
+pieSeries.labels.template.radius = 3;
+pieSeries.labels.template.padding(0, 0, 0, 0);
+
+pieSeries.ticks.template.disabled = true;
+
+// Create a base filter effect (as if it's not there) for the hover to return to
+var shadow = pieSeries.slices.template.filters.push(new am4core.DropShadowFilter());
+shadow.opacity = 0;
+
+// Create hover state
+var hoverState = pieSeries.slices.template.states.getKey("hover"); // normally we have to create the hover state, in this case it already exists
+
+// Slightly shift the shadow and make it more prominent on hover
+var hoverShadow = hoverState.filters.push(new am4core.DropShadowFilter());
+hoverShadow.opacity = 0.7;
+hoverShadow.blur = 5;
+
+// Add a legend
+chart.legend = new am4charts.Legend();
+
+chart.data = [
+            <?php
+            foreach($generos as $genero){
+                echo "{'country':'" . $genero->raca . "','litres':'" . $genero->qtd . "'},";
+            };
+            ?>
+
+];
+  </script>
+
+
+
+
 </body>
 </html>
