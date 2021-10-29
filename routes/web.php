3f902a22dde776e7916d50302f181080a87f519c
Alexandre Tucunduva <motive.mobi@gmail.com>
2021-10-29 08:17:45 -0300

    protegendo a rota '/dashboard'

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::middleware(['auth'])->group(function () {
+  Route::get('/dashboard', function () {
+      return view('dashboard');
+  });
 });
 
 
